dfb0e5ec0cd952d6e6658d837d2b72ddfca1b90c
unknown <fadonic@yahoo.com>
2022-01-21 10:11:34 +0100

    Initial commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = require('express')()
 const PORT = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
-  res.json({ success: true, data: 'Hello from express' })
+  res.json({ success: true, data: 'Hello from express', status: 'ok' })
 })
 
 app.listen(PORT, () => {
